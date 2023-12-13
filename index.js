1a3014a5521d70c82b63f6cd0579e3bb43e0e7cb
Aethan Ilagan <aethan.ilagan@ubx.ph>
2023-12-13 13:16:10 +0800

    Added uploads

--- index.js
+++ index.js
@@ -5,16 +5,29 @@ import open from "open";
 import getServerIp from "./utils/getServerIp.js";
 import express from "express";
 import morgan from "morgan";
+import multer from "multer";
+import path from "path";
+import fs from "fs";
+import { exec } from "child_process";
 
 const app = express();
 const port = 3000; // You can choose any port that's open
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  // morgan(":method :url :status :res[content-length] - :response-time ms")
-  morgan("combined")
-);
+app.use(morgan("combined"));
+
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, "uploads/");
+  },
+  filename: (req, file, callback) => {
+    const fileName =
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname);
+    callback(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
 
 app.get("/", (req, res) => {
   return res.json({
@@ -26,7 +39,7 @@ app.post("/text", (req, res) => {
   const { content } = req.body;
   const deviceName = req.body.device_name ?? "Device";
 
-  console.log("Data received from iPhone:", req.body);
+  console.log(`Data received from ${deviceName}: ${req.body}`);
 
   clipboardy.writeSync(content);
 
@@ -41,6 +54,46 @@ app.post("/text", (req, res) => {
   }
   console.log("Notification sent!");
   return res.json({ success: true });
+});
+
+app.post("/image", upload.single("file"), (req, res) => {
+  const deviceName = req.body.device_name ?? "Device";
+  if (req.file) {
+    console.log(
+      `File from ${deviceName} has been uploaded successfully: ${req.file}`
+    );
+
+    const imagePath = `uploads/${req.file.originalname}`;
+    console.log(imagePath);
+
+    let command;
+    switch (process.platform) {
+      case "win32":
+        command = `Powershell -command "Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.Clipboard]::SetImage([System.Drawing.Image]::FromFile('${imagePath}'))"`;
+        break;
+      case "darwin":
+        command = `osascript -e 'tell application "Finder" to set the clipboard to ( POSIX file "${imagePath}" )'`;
+        break;
+      case "linux":
+        command = `xclip -selection clipboard -t image/png -i ${imagePath}`;
+        break;
+      default:
+        return res.status(500).send("Unsupported OS");
+      case "win32":
+        command = "";
+    }
+
+    exec(command, (error) => {
+      if (error) {
+        console.error(`Error: ${error}`);
+        return res.status(500).json({ success: false });
+      }
+      return res.status(200).json({ success: true });
+    });
+  } else {
+    console.log(`No file from ${deviceName} uploaded`);
+    res.status(400).json({ success: false });
+  }
 });
 
 // const server = http.createServer((req, res) => {
