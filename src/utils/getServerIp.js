f44b2bfe9a9b86dd5294d9685b4283692dfced80
Thanmatt <me@thanmatt.me>
2025-01-26 17:55:41 +0800

    Updates to use pnpm

--- src/utils/getServerIp.js
+++ src/utils/getServerIp.js
@@ -1,26 +1,27 @@
-import os from "os";
-import getMacIp from "./getMacIp.js";
+import os from "os"
+import getMacIp from "./getMacIp.js"
 export default function getServerIp() {
-  const platform = os.platform();
-  const interfaces = os.networkInterfaces();
+  const platform = os.platform()
+  const interfaces = os.networkInterfaces()
   // The name of the WiFi interface can vary. Common names include 'Wi-Fi' and 'wlan0'.
-  let wifiInterfaceName;
+  let wifiInterfaceName
 
-  console.log(platform);
+  console.log(platform)
   // :: Mac
   if (platform === "darwin") {
-    return getMacIp(interfaces);
+    return getMacIp(interfaces)
   } else {
-    wifiInterfaceName = "Wi-Fi"; // Change this to match your WiFi interface name
+    console.log("🚀 ~ getServerIp ~ interfaces:", interfaces)
+    wifiInterfaceName = "Wi-Fi" // Change this to match your WiFi interface name
     if (interfaces[wifiInterfaceName]) {
-      console.log(wifiInterfaceName);
+      console.log(wifiInterfaceName)
       for (const iface of interfaces[wifiInterfaceName]) {
         if (iface.family === "IPv4" && !iface.internal) {
-          return iface.address;
+          return iface.address
         }
       }
     }
   }
 
-  return "127.0.0.1"; // Fall
+  return "127.0.0.1" // Fall
 }
